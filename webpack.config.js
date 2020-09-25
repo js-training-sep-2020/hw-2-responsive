86cfb45d39e984cb1fc1921941a7a62fba51c5ae
Gina <e.kurmeneva@gmail.com>
2020-09-25 18:41:47 +0300

    Add basic styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const miniCss = require('mini-css-extract-plugin');
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
