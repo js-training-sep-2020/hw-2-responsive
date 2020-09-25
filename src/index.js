13d20cd26a4a301c8f848b89cbe043a61237cb9f
Gina <e.kurmeneva@gmail.com>
2020-09-25 17:28:18 +0300

    Add file-loader and load import images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import './scss/app.scss';
+import './scss/weather-widget-flex.scss';
+
+import './img/bg_paris.jpg';
+import './img/icon_arrow_left.png';
+import './img/icon_arrow_right.png';
+import './img/icon_weather_cloudy.png';
+import './img/icon_weather_overcast.png';
+import './img/icon_weather_rain.png';
+import './img/icon_weather_sunny.png';
+import './img/icon_wind.png';
