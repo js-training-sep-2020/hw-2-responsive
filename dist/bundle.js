289e40fcddb45a6451d62ca22df421469b12e4c6
Gina <e.kurmeneva@gmail.com>
2020-09-28 11:28:38 +0300

    Refactor working directory
    Move reusable mixins to the separate file
    Add dist's files to Git
    Rename carousel.scss to carousel-flex.scss

---
+++ dist/bundle.js
@@ -0,0 +1,208 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/img/bg_paris.jpg":
+/*!******************************!*\
+  !*** ./src/img/bg_paris.jpg ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"40d4231f20e8d29be851005b789561bb.jpg\");\n\n//# sourceURL=webpack:///./src/img/bg_paris.jpg?");
+
+/***/ }),
+
+/***/ "./src/img/icon_arrow_left.png":
+/*!*************************************!*\
+  !*** ./src/img/icon_arrow_left.png ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"937967d47c4d111df160a4768fc4fb73.png\");\n\n//# sourceURL=webpack:///./src/img/icon_arrow_left.png?");
+
+/***/ }),
+
+/***/ "./src/img/icon_arrow_right.png":
+/*!**************************************!*\
+  !*** ./src/img/icon_arrow_right.png ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"b6766c65d34dd82dd6289e055b90aebe.png\");\n\n//# sourceURL=webpack:///./src/img/icon_arrow_right.png?");
+
+/***/ }),
+
+/***/ "./src/img/icon_weather_cloudy.png":
+/*!*****************************************!*\
+  !*** ./src/img/icon_weather_cloudy.png ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"66f9fed9433227c7e230cafa57ecb8a6.png\");\n\n//# sourceURL=webpack:///./src/img/icon_weather_cloudy.png?");
+
+/***/ }),
+
+/***/ "./src/img/icon_weather_overcast.png":
+/*!*******************************************!*\
+  !*** ./src/img/icon_weather_overcast.png ***!
+  \*******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"190c3203efec2f5472f34aa00749333e.png\");\n\n//# sourceURL=webpack:///./src/img/icon_weather_overcast.png?");
+
+/***/ }),
+
+/***/ "./src/img/icon_weather_rain.png":
+/*!***************************************!*\
+  !*** ./src/img/icon_weather_rain.png ***!
+  \***************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"8c37c1fbd415ee324e265c1f1328048a.png\");\n\n//# sourceURL=webpack:///./src/img/icon_weather_rain.png?");
+
+/***/ }),
+
+/***/ "./src/img/icon_weather_sunny.png":
+/*!****************************************!*\
+  !*** ./src/img/icon_weather_sunny.png ***!
+  \****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"661909b81406f82cea70e80335520643.png\");\n\n//# sourceURL=webpack:///./src/img/icon_weather_sunny.png?");
+
+/***/ }),
+
+/***/ "./src/img/icon_wind.png":
+/*!*******************************!*\
+  !*** ./src/img/icon_wind.png ***!
+  \*******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"0abd0130f762627ddf6ff2eee508f301.png\");\n\n//# sourceURL=webpack:///./src/img/icon_wind.png?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_weather_widget_flex_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/weather-widget-flex.scss */ \"./src/scss/weather-widget-flex.scss\");\n/* harmony import */ var _scss_weather_widget_flex_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_weather_widget_flex_scss__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _img_bg_paris_jpg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./img/bg_paris.jpg */ \"./src/img/bg_paris.jpg\");\n/* harmony import */ var _img_icon_arrow_left_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./img/icon_arrow_left.png */ \"./src/img/icon_arrow_left.png\");\n/* harmony import */ var _img_icon_arrow_right_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./img/icon_arrow_right.png */ \"./src/img/icon_arrow_right.png\");\n/* harmony import */ var _img_icon_weather_cloudy_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./img/icon_weather_cloudy.png */ \"./src/img/icon_weather_cloudy.png\");\n/* harmony import */ var _img_icon_weather_overcast_png__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./img/icon_weather_overcast.png */ \"./src/img/icon_weather_overcast.png\");\n/* harmony import */ var _img_icon_weather_rain_png__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./img/icon_weather_rain.png */ \"./src/img/icon_weather_rain.png\");\n/* harmony import */ var _img_icon_weather_sunny_png__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./img/icon_weather_sunny.png */ \"./src/img/icon_weather_sunny.png\");\n/* harmony import */ var _img_icon_wind_png__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./img/icon_wind.png */ \"./src/img/icon_wind.png\");\n\n\n\n\n\n\n\n\n\n\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/scss/weather-widget-flex.scss":
+/*!*******************************************!*\
+  !*** ./src/scss/weather-widget-flex.scss ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/scss/weather-widget-flex.scss?");
+
+/***/ })
+
+/******/ });
